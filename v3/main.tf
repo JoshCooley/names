1fe0187f1f4a4ca115406c4d5d3859269689866e
Josh Cooley <cooley.josh@gmail.com>
2020-02-26 11:23:14 -0600

    Initial commit

---
+++ v3/main.tf
@@ -0,0 +1,48 @@
+variable "first_names" {
+  type = "list"
+}
+
+variable "middle_initials" {
+  type = "list"
+}
+
+variable "last_names" {
+  type = "list"
+}
+
+variable "titles" {
+  type    = "list"
+  default = []
+}
+
+resource "null_resource" "first_names" {
+  count = "${length(var.first_names)}"
+
+  provisioner "local-exec" {
+    command = "echo ${var.first_names[count.index]}"
+  }
+}
+
+resource "null_resource" "middle_initials" {
+  count = "${length(var.middle_initials)}"
+
+  provisioner "local-exec" {
+    command = "echo ${var.middle_initials[count.index]}"
+  }
+}
+
+resource "null_resource" "last_names" {
+  count = "${length(var.last_names)}"
+
+  provisioner "local-exec" {
+    command = "echo ${var.last_names[count.index]}"
+  }
+}
+
+resource "null_resource" "titles" {
+  count = "${length(var.titles)}"
+
+  provisioner "local-exec" {
+    command = "echo ${var.titles[count.index]}"
+  }
+}
